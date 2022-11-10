09ac30508bed5a5b705c7de78f1884b34031481f
unknown <avanimodi4@gmail.com>
2022-11-10 21:26:20 +1300

    first commit

---
+++ august2022/page/homepage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using August2022.Utilities;
+using OpenQA.Selenium;
+
+namespace August2022.Pages
+{
+    public class HomePage
+    {
+        public void GoToTMPage(IWebDriver driver)
+        {
+            // Navigate to Time & Materials page
+            IWebElement administrationTab = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+            administrationTab.Click();
+
+            WaitHelpers.WaitToBeClickable(driver, "XPath", "/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a", 3);
+            
+            IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            tmOption.Click();
+        }
+    }
+}
